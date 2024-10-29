bae23328a3a41e0bcbde36360b0cd3f21b50c676
devsumarjunaid <145253338+SudoW1n@users.noreply.github.com>
2024-10-30 02:56:21 +0530

    pushing it to git

---
+++ conn.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$database = "vaceyra";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+
+?>
